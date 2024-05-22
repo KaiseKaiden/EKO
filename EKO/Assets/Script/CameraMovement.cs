199f050cf3304de2e14d643a642cf81d3ee20b99
KaiseKaiden <LexFrost01@hotmail.com>
2024-05-22 13:30:21 +0200

    Player movement

---
+++ EKO/Assets/Script/CameraMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] float myCameraSpeed;
+    [SerializeField] float myMaxCameraDistance;
+    [SerializeField] float myRightDistance;
+    [SerializeField] float myUpDistance;
+    [SerializeField] float myClampAngle;
+
+    [SerializeField] LayerMask myCameraLayer;
+
+    float myCurrentDistance;
+    float myDeciredDistance;
+
+    float lookX;
+    float lookY;
+
+    private void Start()
+    {
+        myCurrentDistance = myMaxCameraDistance;
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Mouse X");
+        float inputY = Input.GetAxis("Mouse Y");
+
+        lookX += -inputY * myCameraSpeed * Time.deltaTime;
+        lookY += inputX * myCameraSpeed * Time.deltaTime;
+        lookX = Mathf.Clamp(lookX, -myClampAngle, myClampAngle);
+        transform.eulerAngles = new Vector3(lookX, lookY, 0.0f);
+
+        SetCurrentDistance();
+
+        Vector3 centerPosition = Vector3.up * myUpDistance + transform.right * myRightDistance;
+        transform.localPosition = centerPosition - transform.forward * myCurrentDistance;
+    }
+
+    void SetCurrentDistance()
+    {
+        Vector3 centerPosition = Vector3.up * myUpDistance + transform.right * myRightDistance;
+
+        RaycastHit hit;
+        if (Physics.Raycast(centerPosition, -transform.forward, out hit, myMaxCameraDistance, myCameraLayer))
+        {
+            myDeciredDistance = hit.distance;
+            //if (myCurrentDistance > myDeciredDistance)
+            //{
+            //    myCurrentDistance = myDeciredDistance;
+            //}
+        }
+        else
+        {
+            myDeciredDistance = myMaxCameraDistance;
+        }
+
+        myCurrentDistance = myDeciredDistance;// Mathf.Lerp(myCurrentDistance, myDeciredDistance, 2.0f * Time.deltaTime);
+    }
+}
---
+++ EKO/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float myWalkingSpeed;
+    [SerializeField] float myRotationSpeed;
+
+    Transform myCameraTransform;
+    CharacterController myCharacterController;
+
+    Vector3 myDeciredForward;
+
+    void Start()
+    {
+        myCameraTransform = Camera.main.transform;
+        myCharacterController = GetComponent<CharacterController>();
+
+        myDeciredForward = transform.forward;
+    }
+
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+
+    void Movement()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 forward = myCameraTransform.forward;
+        forward.y = 0.0f;
+        forward.Normalize();
+
+        Vector3 right = Vector3.Cross(Vector3.up, forward);
+        Vector3 direction = ((right * inputX) + (forward * inputY)).normalized;
+
+        myCharacterController.Move(direction * myWalkingSpeed * Time.deltaTime);
+
+        if (Mathf.Abs(inputX) > 0 || Mathf.Abs(inputY) > 0)
+        {
+            myDeciredForward = direction;
+        }
+    }
+
+    void Rotation()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(myDeciredForward), myRotationSpeed * Time.deltaTime);
+    }
+}
