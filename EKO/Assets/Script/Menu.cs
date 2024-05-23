5ba24410986a18a59687aa3695bb0f557cb59770
liljaliten <sabinaspel@gmail.com>
2024-05-23 12:02:30 +0200

    Fade Transition

--- EKO/Assets/Script/Menu.cs
+++ EKO/Assets/Script/Menu.cs
@@ -6,11 +6,27 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     [SerializeField] GameObject credits;
+    [SerializeField] Animator transitionAnimator;
+    [SerializeField] GameObject transitionCanvas;
 
-    public void PlayGame()
+    private void Start()
     {
+        transitionAnimator = GetComponent<Animator>();
+
+    }
+
+ 
+    public IEnumerator PlayGame()
+    {
+        transitionCanvas.SetActive(true);
+        transitionAnimator.SetTrigger("TriggerBlackFade");
+        Debug.Log("Fading in");
+
+        yield return new WaitForSeconds(1);
+
         Debug.Log("Game is Playing");
         SceneManager.LoadScene("Level1");
+        transitionCanvas.SetActive(false);
     }
 
     public void OpenCredits()
@@ -28,4 +44,24 @@ public class Menu : MonoBehaviour
         Debug.Log("Game is Quitting");
         Application.Quit();
     }
+
+    IEnumerator TransitionBlack()
+    {
+        transitionCanvas.SetActive(true);
+        transitionAnimator.SetTrigger("TriggerBlackFade");
+
+        yield return new WaitForSeconds(1);
+
+        transitionCanvas.SetActive(false);
+    }
+    IEnumerator TransitionWhite()
+    {
+        transitionCanvas.SetActive(true);
+        transitionAnimator.SetTrigger("TriggerWhiteFade");
+
+        yield return new WaitForSeconds(1);
+
+        transitionCanvas.SetActive(false);
+    }
+
 }
